4321d705187f5f8da04c9f09754816eb53cab060
Gu1hx <guilhermex2622@gmail.com>
2026-04-29 18:46:52 -0300

    feat: added create-meal migration

--- db/migrations/20260429213552_create-meals.ts
+++ db/migrations/20260429213552_create-meals.ts
@@ -1,14 +1,17 @@
 import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-	return knex.schema.createTable("users", (table) => {
+	return knex.schema.createTable("meals", (table) => {
 		table.uuid("id").primary();
 		table.uuid("session_id");
 		table.text("name").notNullable();
+		table.text("description");
+		table.datetime("datetime").notNullable();
+		table.boolean("is_on_diet").notNullable();
 		table.timestamp("created_at").defaultTo(knex.fn.now()).notNullable();
 	});
 }
 
 export async function down(knex: Knex): Promise<void> {
-	return knex.schema.dropTable("users");
+	return knex.schema.dropTable("meals");
 }
